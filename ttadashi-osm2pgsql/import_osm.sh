6132c4f80111dba8a485b9c27899a2931219854b
Julien Jacquelinet <julien.jacquelinet@gmail.com>
2025-05-04 18:17:54 +0200

    actually import osm data

--- ttadashi-osm2pgsql/import_osm.sh
+++ ttadashi-osm2pgsql/import_osm.sh
@@ -4,14 +4,14 @@ PG_USER="gisuser"
 PG_PASSWORD="gispwd"
 PG_DB="gis"
 PG_HOST="postgis"
-OSM_FILE="/osm/data.osm.pbf"
+OSM_FILE="/import/data.osm.pbf"
 
 echo "Importation des données OpenStreetMap..."
 
-osm2pgsql --create --slim -d $PG_DB -U $PG_USER -H $PG_HOST \
+PGPASSWORD="$PG_PASSWORD" osm2pgsql --create --slim -d "$PG_DB" -U "$PG_USER" -H "$PG_HOST" \
     --hstore --cache 1000 --number-processes 4 \
-    --style default.style \
+    --style railways.style \
     --output pgsql \
-    $OSM_FILE
+    "$OSM_FILE"
 
 echo "Importation terminée !"
